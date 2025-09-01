d97dc4abf3c9c7e385c5994a92336b6b6379640d
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-09-01 20:20:49 +0000

    ad

--- tratra/celery.py
+++ tratra/celery.py
@@ -2,7 +2,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tratra.settings.prod")  # ou .dev selon ton env
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tratra.settings")  # ou .dev selon ton env
 
 app = Celery("tratra")
 
--- tratra/settings/__init__.py
+++ tratra/settings/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-env = os.environ.get('DJANGO_ENV', 'dev')
+env = os.environ.get('DJANGO_ENV')
 
 if env == 'prod':
     from .prod import *
